a00f120cb1e26fe3064e767f09d0d0137bea62e7
LucaCiani <lucaciani88@gmail.com>
2025-06-17 14:14:04 +0200

    complete + bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,9 @@ fetch("https://lanciweb.github.io/demo/api/pictures/")
     .then((data) => {
         data.forEach((element) => {
             rowEl.innerHTML += `<div class="col">
-    <div class="card">
-        <div>
-            <img src="${element.url}" alt="${element.title}" />
-        </div>
+    <div class="card shadow-lg">
+        <img class="pin" src="./assets/img/pin.svg" alt="pin">
+        <img src="${element.url}" alt="${element.title}" />
         <div class="card-body">
             <h5 class="card-title">${element.title}</h5>
         </div>
